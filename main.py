a5d79e0a2552237beb6f54be3cc1d277d1e25762
disc3 <33780466+disc3@users.noreply.github.com>
2025-12-24 16:17:12 +0100

    refactor get_word_count function to separate file

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
+from stats import get_word_count
+
 def get_book_text(filepath):
     with open(filepath, encoding='utf-8') as bookfile:
         book_contents = bookfile.read()
     return book_contents
 
-def get_word_count(book_content):
-    word_count = len(book_content.split())
-    return print(f"Found {word_count} total words")
-    
 def main():
     filepath = "./books/frankenstein.txt"
     content = get_book_text(filepath)
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_word_count(book_content):
+    word_count = len(book_content.split())
+    return print(f"Found {word_count} total words")
