0b3f538faf8a7ead0a8bba14893cd684afd6c27a
disc3 <diegoschaich@web.de>
2025-12-25 12:22:47 +0100

    change filepath to be included as parameter

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_word_count, print_sorted_count
 
 def get_book_text(filepath):
@@ -6,7 +7,11 @@ def get_book_text(filepath):
     return book_contents
 
 def main():
-    filepath = "./books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    filepath = sys.argv[1]
     content = get_book_text(filepath)
     print(f"Found {get_word_count(content)} total words")
     print(print_sorted_count(content))
